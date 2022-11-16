179cd467d23af27f21a9083d3bcaee92a296fcfd
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-11-16 10:27:38 +0100

    UPDATE: logger

--- PythonLogger/logger.py
+++ PythonLogger/logger.py
@@ -2,7 +2,7 @@ import logging
 from enum import Enum
 
 
-class _Levels(Enum):
+class Levels(Enum):
     Debug = logging.DEBUG
     Info = logging.INFO
     Error = logging.ERROR
@@ -10,7 +10,7 @@ class _Levels(Enum):
 
 def set_logger(
     *,
-    level: _Levels = _Levels.Info.value,
+    level: Levels = Levels.Info.value,
     format: str = "[%(levelname)s] %(asctime)s - %(message)s"
 ) -> None:
     logging.basicConfig(level=level, format=format)
